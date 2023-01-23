4b72b103d861c2cf3f19c1b6b5a1580c268b8d7b
Dea Indri Kusuma <121590653+deaindrik@users.noreply.github.com>
2023-01-23 08:47:17 +0700

    Add files via upload

---
+++ app/views/partials/appfooter.php
@@ -0,0 +1,11 @@
+<footer class="footer border-top">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-5">
+				<div class="copyright">Kantor Kecamatan Margoyoso Pati | &copy; <?php echo SITE_NAME ?> - <?php echo date('Y') ?></div>
+			</div>
+			</div>
+			
+		</div>
+	</div>
+</footer>

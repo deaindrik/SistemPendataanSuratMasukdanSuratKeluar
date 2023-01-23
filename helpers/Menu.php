cf7273a0f903c9323a7c50ee82d14b0744f18057
Dea Indri Kusuma <121590653+deaindrik@users.noreply.github.com>
2023-01-23 08:48:17 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'surat_masuk', 
+			'label' => 'Surat Masuk', 
+			'icon' => '<i class="fa fa-inbox "></i>'
+		),
+		
+		array(
+			'path' => 'surat_keluar', 
+			'label' => 'Surat Keluar', 
+			'icon' => '<i class="fa fa-mail-forward "></i>'
+		),
+	);
+		
+	
+	
+}
